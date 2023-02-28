ff51abba4ab565f5db22d4adb75ab9f6063ce7cc
Bogdan Didyk <bogdandidyk26@gmail.com>
2023-02-28 23:50:17 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,51 @@
+function getRandomSortedArray(length, min = 0, max = 9) {
+    const arr = [];
+
+    for (let i = 0; i < length; i++) {
+        arr[i] = Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    return arr.sort((a, b) => a - b);
+}
+
+function mergeTwoSortedArrays(arr1, arr2) {
+    const len1 = arr1.length;
+    const len2 = arr2.length;
+    const result = [];
+    let ind1 = 0;
+    let ind2 = 0;
+
+    while (ind1 != len1 && ind2 != len2) {
+        if (arr1[ind1] <= arr2[ind2]) {
+            result.push(arr1[ind1]);
+            ind1++;
+        }
+        else {
+            result.push(arr2[ind2]);
+            ind2++;
+        }
+    }
+
+    if (ind1 == len1) {
+        for (ind2; ind2 < len2; ind2++) {
+            result.push(arr2[ind2]);
+        }
+    }
+    
+    if (ind2 == len2) {
+        for (ind1; ind1 < len1; ind1++) {
+            result.push(arr1[ind1]);
+        }
+    }
+
+    return result;
+}
+
+const arr1 = getRandomSortedArray(7);
+const arr2 = getRandomSortedArray(5, -3, 7);
+console.log("arr1 =", arr1);
+console.log("arr2 =", arr2);
+console.log("");
+
+const res = mergeTwoSortedArrays(arr1, arr2);
+console.log("result =", res);
